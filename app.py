9fd19044456480f20f2663059781b1ddb7471ca9
Sardorbek <96407270+Chaqmoqjon@users.noreply.github.com>
2025-02-12 17:42:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import platform
 plt = platform.system()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
-st.title('Mevalarni ajratadigan AI')
+st.title('Olma, uzum, banan, qulupnay va mandarinni ajratadigan AI')
 
 file = st.file_uploader("Rasm yuklash", ['png', 'jpg', 'jpeg', 'gif'])
 
@@ -20,6 +20,6 @@ if file:
     pred, pred_idx, prob = model.predict(img)
     st.image(img)
     st.success(f"Tahmin: {pred}")
-    st.info(f"Koffesienti: {prob[pred_idx] * 100:.2f}")
+    st.info(f"Koffesienti: {prob[pred_idx] * 100:.2f}%")
     fig = px.bar(x=prob * 100, y=model.dls.vocab, labels={'x': 'Ehtimol (%)', 'y': 'Toifalar'}, title='Ehtimollar diagrammasi')
     st.plotly_chart(fig)
