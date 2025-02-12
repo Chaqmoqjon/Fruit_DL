1fb7243904b6f6755393b7878b0eb81b64f93073
Sardorbek <chaqmoqjon@gmail.com>
2025-02-12 17:32:40 +0500

    version: 0.0.1

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+import platform
+
+# temp = pathlib.PosixPath
+# pathlib.PosixPath = pathlib.WindowsPath
+
+plt = platform.system()
+if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
+
+st.title('Mevalarni ajratadigan AI')
+
+file = st.file_uploader("Rasm yuklash", ['png', 'jpg', 'jpeg', 'gif'])
+
+if file:
+    img = PILImage.create(file)
+    model = load_learner('fruits_model.pkl')
+    pred, pred_idx, prob = model.predict(img)
+    st.image(img)
+    st.success(f"Tahmin: {pred}")
+    st.info(f"Koffesienti: {prob[pred_idx] * 100:.2f}")
+    fig = px.bar(x=prob * 100, y=model.dls.vocab, labels={'x': 'Ehtimol (%)', 'y': 'Toifalar'}, title='Ehtimollar diagrammasi')
+    st.plotly_chart(fig)
